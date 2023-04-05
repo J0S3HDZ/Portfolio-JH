b403387adea8e9067ec203b40ba99bc80b4e6a67
J0S3HDZ <josehdzoficial@gmail.com>
2023-04-04 22:03:01 -0600

    v2.0 CV 2K23 JJHM

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+    window.addEventListener('load', () => {
+        const fr = document.querySelector('iframe').style.display = 'none';
+
+        const elements1 = document.querySelectorAll('#left_op');
+        const reversedElements = Array.prototype.slice.call(elements1).reverse();
+        reversedElements.forEach((element, index) => {
+            setTimeout(() => {
+                element.classList.add('moveUp');
+            }, (index + 1) * 300);
+        });
+
+        const elements2 = document.querySelectorAll('#right_op');
+        elements2.forEach((elementr, index) => {
+        setTimeout(() => {
+            elementr.classList.add('moveUp');
+        }, (index + 1) * 300);
+        });
+        
+        const logo = document.getElementById('logo_op');
+        logo.classList.add('moveUp');
+    });
+    function scrollToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
